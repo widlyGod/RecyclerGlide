98d340d2047df8c54819453f01ccf7ddaa09ab43
洪扬 <hongyang@interheart.cn>
2019-11-27 00:00:23 +0800

    update

--- app/src/main/java/com/example/myapplication/MainAdapter.kt
+++ app/src/main/java/com/example/myapplication/MainAdapter.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication
 
+import android.graphics.Bitmap
 import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+import com.bumptech.glide.request.target.SimpleTarget
 import com.chad.library.adapter.base.BaseQuickAdapter
 import com.chad.library.adapter.base.BaseViewHolder
 
